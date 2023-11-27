105ba100b4ddc43055fc367bb858af0eb4b3cf05
Ankan Bhaumik <ankanbhaumik80@gmail.com>
2023-11-27 20:34:34 +0530

    test

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ app.use(
   cors({
     origin: ["http://localhost:3000", "https://student-enrolment.netlify.app"],
     credentials: true,
+    allowedHeaders: true,
     methods: "GET,POST,PUT,DELETE",
   })
 );
